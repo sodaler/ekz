84f9c91e6b850a1809aad56ad0b8e09c8d595345
Nelikey <nelikey@yandex.ru>
2024-05-27 17:39:16 +0400

    tp_migration

---
+++ ExamMigration/ExamMigration/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamMigration
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int RoomNumber { get; set; }
+
+        public Patient()
+        {
+
+        }
+
+        public Patient(string name, int age, int roomNumber)
+        {
+            Name = name;
+            Age = age;
+            RoomNumber = roomNumber;
+        }
+    }
+}
---
+++ ExamMigration/ExamMigration/PatientDataBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace ExamMigration
+{
+    internal class PatientDataBase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder != null)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Database;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual Microsoft.EntityFrameworkCore.DbSet<Patient> Patients { set; get; }
+    }
+}
